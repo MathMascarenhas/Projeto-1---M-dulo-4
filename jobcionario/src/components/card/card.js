35ca8a3a625ce1df8f55d828c365ff54f77c78d3
Matheus Mascarenhas <mathmasca@gmail.com>
2022-10-04 22:49:05 +0100

    #

--- jobcionario/src/components/card/card.js
+++ jobcionario/src/components/card/card.js
@@ -1,6 +1,6 @@
 import "./card.css";
 
-export function Card({word}) {
+export function Card({ word }) {
   return (
     <div className="card-component">
       <h2 className="card-title">{word}</h2>
--- jobcionario/src/components/form/form.js
+++ jobcionario/src/components/form/form.js
@@ -2,12 +2,12 @@ import "./form.css";
 import { api } from "../../utils/api/api";
 import { useState } from "react";
 
-export function Form({ handleModal, getAll}) {
+export function Form({ handleModal, getAll }) {
   const [newWord, setNewWord] = useState({});
 
   async function handleSubmit(event) {
     event.preventDefault();
-    const word = JSON.stringify(newWord)
+    const word = JSON.stringify(newWord);
     await api.createWord(word);
     await getAll();
     handleModal();
@@ -19,21 +19,36 @@ export function Form({ handleModal, getAll}) {
         <h2 className="title-form">Preencha os Campos</h2>
         <section className="form-section">
           <label className="form-label">Palavra</label>
-          <input type="text" name="word" className="form-input" onChange={(event) => {
-                  setNewWord({newWord, word: event.target.value });
-                }}></input>
+          <input
+            type="text"
+            name="word"
+            className="form-input"
+            onChange={(event) => {
+              setNewWord({ newWord, word: event.target.value });
+            }}
+          ></input>
         </section>
         <section className="form-section">
           <label className="form-label">Tradução</label>
-          <input type="text" name="translation" className="form-input" onChange={(event) => {
-                  setNewWord({newWord, translation: event.target.value });
-                }}></input>
+          <input
+            type="text"
+            name="translation"
+            className="form-input"
+            onChange={(event) => {
+              setNewWord({ newWord, translation: event.target.value });
+            }}
+          ></input>
         </section>
         <section className="form-section">
           <label className="form-label">Definição</label>
-          <input type="text" name="definition" className="form-input" onChange={(event) => {
-                  setNewWord({newWord, definition: event.target.value });
-                }}></input>
+          <input
+            type="text"
+            name="definition"
+            className="form-input"
+            onChange={(event) => {
+              setNewWord({ newWord, definition: event.target.value });
+            }}
+          ></input>
         </section>
         <button type="submit" className="btn-submit">
           Criar
